239dc4b6d90028de58442860a7663610ae44f01a
Katy Justiss <katy.justiss@gmail.com>
2015-06-03 14:50:59 -0500

    added modal form

--- src/js/main.js
+++ src/js/main.js
@@ -5,11 +5,11 @@ angular
     var main = this;
 
     main.people = [
-      {name: 'Wiggles', email: 'wig@wiggles.com', twitter: '@wiggly'},
-      {name: 'Sunshine', email: 'sun@sunny.com', twitter: '@sunny'},
-      {name: 'Pumpkin', email: 'orange@pumpkin.com', twitter: '@plumpy'},
-      {name: 'Princess', email: 'pink@princess.com', twitter: '@princess'},
-      {name: 'Mario', email: 'mario@plumbers.com', twitter: '@mario'},
+      {name: 'Wiggles', email: 'wig@wiggles.com', twitter: '@wiggly', phone: '6155555678', photo: 'http://i.imgur.com/lVlPvCBb.jpg'},
+      {name: 'Sunshine', email: 'sun@sunny.com', twitter: '@sunny', phone: '615-123-4567', photo: 'http://i.imgur.com/EpTz5rOb.jpg'},
+      {name: 'Pumpkin', email: 'orange@pumpkin.com', twitter: '@plumpy', phone: '6152345678', photo: 'http://i.imgur.com/Z9809Jeb.jpg'},
+      {name: 'Princess', email: 'pink@princess.com', twitter: '@princess', phone: '615-666-7890', photo: 'http://i.imgur.com/PISWBXMb.jpg'},
+      {name: 'Mario', email: 'mario@plumbers.com', twitter: '@mario', phone: '6159876543', photo: 'http://i.imgur.com/w31FmCJb.jpg'}
     ];
 
     main.newContact = {};
@@ -17,6 +17,7 @@ angular
     main.addNewContact = function() {
       main.people.push(main.newContact);
       main.newContact = {};  //clearing the form and data-binding link
+      $('#modal').modal('hide');
     };
 
     main.removeAddress = function(contact) {
