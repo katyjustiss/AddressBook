bb56271003152032e064587ae93251070ec97962
Katy Justiss <katy.justiss@gmail.com>
2015-06-08 22:46:11 -0500

    added angular factories and refactored

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,9 @@
 angular
   .module('addressBook', ['ngRoute'])
 
-  .config(function($routeProvider, $locationProvider) {
+  .constant('API_URL', 'https://addressbooks.firebaseio.com')
+
+  .config(function($routeProvider) {
     $routeProvider
       .when('/', {
         templateUrl: 'views/landing.html'
@@ -17,10 +19,20 @@ angular
         controller: 'Main',
         controllerAs: 'main'
       })
+      .when('/people/new', {
+        templateUrl: 'views/people.html',
+        controller: 'NewPersonCtrl',
+        controllerAs: 'main'
+      })
+      .when('/people/:id/edit', {
+        templateUrl: 'views/person.html',
+        controller: 'EditPersonCtrl',
+        controllerAs: 'main'
+      })
       .when('/people/:id', {
         templateUrl: 'views/person.html',
         controller: 'PersonController',
-        controllerAs: 'person'
+        controllerAs: 'main'
       })
       .otherwise({
         templateURL: 'views/404.html'
@@ -40,45 +52,104 @@ angular
       }
     })
 
-  .controller('PersonController', function($http, $routeParams) {
+  //CONTROLLER for individual person's information/data
+  .controller('PersonController', function($routeParams, Person) {
     var main = this;
-    var id = $routeParams.id;
+    main.id = $routeParams.id;
 
-    $http
-      .get(`https://addressbooks.firebaseio.com/people/${id}.json`)
-      .success(function (data) {
-        main.data = data;
+    Person.getOne(main.id, function (data) {
+      main.person = data;
+    })
+    main.onModalLoad = function () {};
+  })
+
+  .factory('Person', function($http, API_URL) {
+    return {
+      getOne(id, cb) { //ES6 enhanced obj literal. Old version - getOne: function(id, cb) {
+        $http
+          .get(`${API_URL}/people/${id}.json`) //returning an obj with a prop of getOne
+          .success(cb);
+      },
+      getAll(cb) {
+        $http
+          .get(`${API_URL}/people/.json`)
+          .success(cb);
+      },
+      create(data, cb) {
+        $http
+          .post(`${API_URL}/people.json`, data)
+          .success(cb);
+      },
+      update(id, data, cb) {
+        $http
+          .put(`${API_URL}/people/${id}.json`, data)
+          .success(cb);
+      },
+      destroy(id, cb) {
+        $http
+          .delete(`${API_URL}/people/${id}.json`)
+          .success(cb);
+      }
+    }
+  })
+
+
+  //CONTROLLER TO ADD PERSON INFORMATION
+  .controller('NewPersonCtrl', function(Person) {
+    var main = this;
+
+    main.onModalLoad = function () {
+      $('#modal').modal('show');
+
+      $('#modal').on('hidden.bs.modal', function (e) {
+        window.location.href = '#/people';
+      });
+    };
+
+    main.saveAddress = function() {
+      Person.create(main.person, function() { //need to pass main.person to the model
+        $('#modal').modal('hide');
+      })
+    };
+      Person.getAll(function (people) {
+        main.people = people;
       });
   })
 
-  .controller('Main', function($http) {
+
+  //CONTROLLER TO EDIT PERSON INFORMATION
+  .controller('EditPersonCtrl', function ($routeParams, Person) {
     var main = this;
+    main.id = $routeParams.id;
 
-    $http
-      .get('https://addressbooks.firebaseio.com/people.json')
-      .success(function (data) {
-        main.people = data;
-      })
+    main.onModalLoad = function () {
+      $('#modal').modal('show');
 
-    main.newContact = {};
-
-    main.addNewContact = function() {
-      $http
-        .post('https://addressbooks.firebaseio.com/people.json', main.newContact)
-        .success(function(res) {
-          main.people[res.name] = main.newContact;
-          main.newContact = {};  //clearing the form and data-binding link
-          $('#modal').modal('hide');
-        });
-    };
-
-    main.removeAddress = function(id) {
-      $http
-        .delete(`https://addressbooks.firebaseio.com/people/${id}.json`)
-        .success(function() {
-          delete main.people[id]
+      $('#modal').on('hidden.bs.modal', function (e) {
+        window.location.href = `#/people/${main.id}`;
       });
     };
+
+    main.saveAddress = function () {
+      Person.update(main.id, main.person, function () {
+        $('#modal').modal('hide');
+      });
+    };
+
+    Person.getOne(main.id, function (person) {
+      main.person = person;
+    });
+  })
+
+  //GETTING ALL THE PEOPLE ON LOAD
+  .controller('Main', function(Person) {
+    var main = this;
+
+    Person.getAll(function(people) {
+      main.people = people;
+    })
+
+    main.onModalLoad = function () {};
   });
 
 
