56b22b49b083e720f3f1fdb3730f739ab59b34a9
Katy Justiss <katy.justiss@gmail.com>
2015-08-04 16:04:33 -0500

    fixed register and added some styling

--- src/assets/auth/login.controller.js
+++ src/assets/auth/login.controller.js
@@ -1,12 +1,11 @@
 angular
   .module('addressBook')
 
-  .controller('LoginCtrl', function ($rootScope, $scope, $location, API_URL) {
+  .controller('LoginCtrl', function ($rootScope, $scope, $location, API_URL, Person) {
     var main = this;
 
     main.login = function () {
       var fb = new Firebase(API_URL);
-
       fb.authWithPassword({
         email: main.email,
         password: main.password
@@ -23,46 +22,20 @@ angular
     };
 
     main.register = function () {
-      // var fb = new Firebase(API_URL);
-      // fb.createUser({
-      //   email: main.email,
-      //   password: main.password
-      // }, function(err, userData) {
-      //   if(err) {
-      //     console.log('Error', err)
-      //   } else {
-      //     main.login();
-      //     console.log('successful')
-      //   }
-      // });
+      var fb = new Firebase(API_URL);
+      fb.createUser({
+        email: main.email,
+        password: main.password
+      }, function(err, userData) {
+        if(err) {
+          console.log('Error', err)
+        } else {
+          Person.create(main.email, main.password, function() {
+            main.login(main.email, main.password);
+            console.log('successful')
+          })
+        };
+      });
     };
+
   });
-
-
-
-
-  // .controller('LoginCtrl', function ($scope, $location, Auth) {
-  //     var main = this;
-
-  //     main.login = function () {
-  //       Auth.login(main.email, main.password, function () {
-  //         $location.path('/people')
-  //         $scope.$apply();
-  //       });
-  //     };
-
-  //     main.register = function () {
-        // var fb = new Firebase(API_URL);
-        // fb.createUser({
-        //   email: main.email,
-        //   password: main.password
-        // }, function(err, userData) {
-        //   if(err) {
-        //     console.log('Error', err)
-        //   } else {
-        //     main.login();
-        //     console.log('successful')
-        //   }
-        // });
-    //   };
-    // });
